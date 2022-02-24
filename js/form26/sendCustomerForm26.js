75d85444c799f08a895d3a5a39f5ef806c636941
Alex <alex1ptb@gmail.com>
2022-02-24 17:27:08 -0500

    message

--- js/form26/sendCustomerForm26.js
+++ js/form26/sendCustomerForm26.js
@@ -7,7 +7,9 @@ function sendForm26(data, pdfURL) {
   let form26ID = pdfURL.match(/[-\w]{25,}(?!.*[-\w]{25,})/);
   console.log(form26ID);
   form26ID = DriveApp.getFileById(form26ID);
-  let subject = `${data["Street"]} - Form 26 Non-Conformity Notice - ${data["Date Of Inspection"]}`;
+  let subject = `${data["Street"]} - Form 26 Non-Conformity Notice - ${data[
+    "Date of Inspection"
+  ].toLocaleDateString()}`;
   let body = `${name}, \n
     Please find attached details of your pool fence/barrier inspection: \n \n
     Address: ${data["Street"]} ${data["Suburb"]} ${data["Local Government Area"]} ${data["Postcode"]} \n
@@ -20,11 +22,26 @@ For any pool barrier remedial/building work it is recommended that you engage a 
 \n
 For any clarification regarding pool safety building/compliance/regulations you may wish to refer to the Building Act 1975, Australian Standards 1926 \n
 Part 1 and Part 2 and QDC MP 3.4, local council requirements and QBCC guidelines:\n
-https://www.legislation.qld.gov.au/view/html/inforce/current/act-1975-011 \n
+<a href="https://www.legislation.qld.gov.au/view/html/inforce/current/act-1975-011">https://www.legislation.qld.gov.au/view/html/inforce/current/act-1975-011</a> \n
 https://www.hpw.qld.gov.au/__data/assets/pdf_file/0015/4812/qdcmp3.4swimmingpoolbarriers.pdf \n
 https://www.standards.org.au/standards-catalogue/sa-snz/building/cs-034/as--1926-dot-1-2012 \n
 https://www.qbcc.qld.gov.au/home-building-owners/pool-safety/overview \n
 Once the changes have been made please contact our office via email to arrange a new inspection:\n`;
+
+  let message = {
+    to: email,
+    subject: subject,
+    htmlBody: body,
+    attachments: [
+      {
+        fileName: `${data["Street"]} - Form 26 Non-conformity Notice.pdf ${data[
+          "Date of Inspection"
+        ].toLocaleDateString()}`,
+        mimeType: "application/pdf",
+        attachment: form26ID,
+      },
+    ],
+  };
 
   GmailApp.createDraft(email, subject, body, {
     attachments: [form26ID],
